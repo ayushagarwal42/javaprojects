96db7ad09f56f6d6d875ad25cfc945556073cd09
Ayush Agarwal <67548077+ayushagarwal42@users.noreply.github.com>
2022-01-18 23:13:08 +0530

    Add files via upload

---
+++ 1st java program/src/com/company/replace.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class replace {
+    public static void main(String[] args) {
+        String name="Ayush Agarwal";
+        System.out.println(name.replace('u','e'));//for all occurence of u
+    }
+}
